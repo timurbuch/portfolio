6284e1c79ef11ef966c7374df30fdba7e4c7c6d2
Timur Buchberger <timur,buch@users.noreplay.github.com>
2021-05-17 18:57:40 +0200

    added transitions for hamburger icon

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,9 @@
 import "./topbar.scss"
 import {MailOutline, PersonOutline} from "@material-ui/icons"
 
-export default function Topbar () {
+export default function Topbar ({menuOpen, setMenuOpen}) {
     return (
-        <div className="topbar " >
+        <div className={"topbar " +(menuOpen && "active")} >
             <div className="wrapper">
                 <div className="left">
                     <a href="#intro" className="logo">genius.</a>
@@ -19,7 +19,7 @@ export default function Topbar () {
                 
                 
                 <div className="right">
-                    <div className="hamburger">
+                    <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
                         <span className="line1"></span>
                         <span className="line2"></span>
                         <span className="line3"></span>
