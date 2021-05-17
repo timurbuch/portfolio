6aef0cc34352621df154986659929ea13a2fd39c
Timur Buchberger <timur,buch@users.noreplay.github.com>
2021-05-17 18:28:26 +0200

    added hamburger menu

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,25 +3,29 @@ import {MailOutline, PersonOutline} from "@material-ui/icons"
 
 export default function Topbar () {
     return (
-        <div className="topbar" id="topbar">
+        <div className="topbar " >
             <div className="wrapper">
                 <div className="left">
                     <a href="#intro" className="logo">genius.</a>
                     <div className="itemContainer">
-                        <PersonOutline/>
+                        <PersonOutline className="icon"/>
                         <span>+49 175 95723961</span>
                     </div>
                     <div className="itemContainer">
-                        <MailOutline/>
+                        <MailOutline className="icon"/>
                         <span>tb@gmail.com</span>
                     </div>
                 </div>
-                </div>
+                
                 
                 <div className="right">
-                    
+                    <div className="hamburger">
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
                 </div>
-            
+            </div>
 
         </div>
     )
