c527d86725cdf701b7fe0bafb48135a9ac7bdf01
Timur Buchberger <timur,buch@users.noreplay.github.com>
2021-05-17 19:09:50 +0200

    added menu component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Intro from "./components/intro/Intro";
 import Portfolio from "./components/portfolio/Portfolio";
 import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
+import Menu from "./components/menu/Menu"
 import "./app.scss"
 import { useState } from "react";
 
@@ -12,6 +13,7 @@ function App() {
   return (
     <div className="app">
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <Menu/>
       <div className="sections">
         <Intro/>
         <Portfolio/>
