07340057d4455bab291c4eb0cfe7bab9ab331275
Timur Buchberger <timur,buch@users.noreplay.github.com>
2021-05-17 17:17:59 +0200

    added material-ui icons

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,16 +1,27 @@
 import "./topbar.scss"
+import {MailOutline, PersonOutline} from "@material-ui/icons"
+
 export default function Topbar () {
     return (
         <div className="topbar" id="topbar">
             <div className="wrapper">
                 <div className="left">
                     <a href="#intro" className="logo">genius.</a>
+                    <div className="itemContainer">
+                        <PersonOutline/>
+                        <span>+49 175 95723961</span>
+                    </div>
+                    <div className="itemContainer">
+                        <MailOutline/>
+                        <span>tb@gmail.com</span>
+                    </div>
+                </div>
                 </div>
                 
                 <div className="right">
                     
                 </div>
-            </div>
+            
 
         </div>
     )
