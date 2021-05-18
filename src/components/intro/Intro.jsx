b6601d4cdef2a099197d10d611eb559e635a6707
Timur Buchberger <timur,buch@users.noreplay.github.com>
2021-05-18 14:04:49 +0200

    added ityped shenanigans

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,5 +1,23 @@
-import "./intro.scss"
+import "./intro.scss";
+import{ init } from "ityped";
+import {useEffect, useRef } from "react";
+
+
 export default function Intro () {
+    
+    const textRef =useRef();
+    
+    useEffect(()=> {
+        init(textRef.current, { 
+            showCursor: true,
+            backDelay: 1500,
+            backSpeed:60, 
+            strings: ['Web Developer', 'Front End Developer', 'App Developer' ],
+            
+        })
+
+    }, [])
+
     return (
         <div className="intro" id="intro">
              <div className="left">
@@ -7,7 +25,14 @@ export default function Intro () {
                      <img src="assets/img_one.jpg" alt=""></img>
                  </div>
              </div>
-             <div className="right"></div>
+             <div className="right">
+                 <div className="wrapper">
+                     <h2>Hi there, I'm</h2>
+                     <h1>Timur Buchberger</h1>
+                     <h3>Freelance <span ref={textRef}></span></h3> 
+                 </div>
+                 <a href="#portfolio"><img src="assets/down.png" alt=""></img></a>
+             </div>
         </div>
     )
 }
